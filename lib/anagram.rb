62b132bca9907acc68cda1b7f29c6a2b05a360b1
Luis Arita <github@luisarita.com>
2017-07-03 14:25:31 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        matches = []
+        words.each do |word|
+            matches << word unless word.chars.sort.join != @word.chars.sort.join
+        end
+        matches
+    end
+end
